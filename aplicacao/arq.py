a69e888cbff13f06decef71245c687e6b40fc74a
Antonio Santana <antcesar.santana@gmail.com>
2016-09-14 20:16:01 -0300

    atualizando

--- aplicacao/arq.py
+++ aplicacao/arq.py
@@ -3,14 +3,23 @@
 
 
 from struct import *
-import run as func
 import os
 
 
-# Funções auxiliadoras
-def isFile(namefile):
+# Funções Auxiliares
+def arquivoExiste(namefile):
     ''' Verifica se o arquivo existe e retorna um valor booleano'''
     return  os.path.exists(namefile)
+
+
+def compactar(chave=0, nome="vazio", idade=0):
+    ''' Retorna uma struct'''
+    return pack("i20sic", chave, nome.encode('utf-8'), idade, '\n'.encode('utf-8'))
+
+
+def descompactar(registro):
+    ''' Retorna um registro com: chave, nome e idade.'''
+    return unpack("i20sic", registro)
 
 
 def escrever_arquivo(namefile, opcao, registro, posicao):
@@ -20,41 +29,38 @@ def escrever_arquivo(namefile, opcao, registro, posicao):
         arquivo.write(registro)
 
 
-def compacta(chave=0, nome="vazio".encode('utf-8'), idade=0):
-    ''' Retorna uma struct formatada: chave, nome e idade.'''
-    return pack("i20sic", chave, nome, idade, '\n'.encode('utf-8'))
-
-
-# Funçoes Principais
-def gravar_registro(namefile, TAM, registro=0, posicao=0):
+# Funções Principais
+def gravar_registro(namefile, TAM, registro, posicao):
     ''' Insere o registro no arquivo e/ou inicia um arquivo.'''
-    if isFile(namefile):
+    if arquivoExiste(namefile):
         escrever_arquivo(namefile, "r+b", registro, posicao)
     else:
         for i in range(TAM):
+            reg = compactar()
             if i != 0:
-                escrever_arquivo(namefile, "r+b", compacta(), i*len(reg))
+                escrever_arquivo(namefile, "r+b", reg, i*len(reg))
             else:
-                escrever_arquivo(namefile, "wb", compacta(), i*len(reg))
-        escrever_arquivo(namefile, "r+b", compacta(), posicao)
+                escrever_arquivo(namefile, "wb", reg, i*len(reg))
+        escrever_arquivo(namefile, "r+b", registro, posicao)
     return True
 
 
-def consultar_registro(namefile, chave, posicao, registro, TAM):
-    with open(namefile,"rb") as arquivo:
-        arquivo.seek(0,2)
-        conteudo = arquivo.tell()
-        tamanho = conteudo / TAM
-    if isFile(namefile):
-        with open(namefile, 'rb') as arquivo:
-            arquivo.seek(position*(tamanho), 0)
-            conteudo = arquivo.read(tamanho)
-            registro = unpack("i20sic",conteudo)
-            if chave == int(registro[0]):
-                print (registro[1]).decode('utf-8'), "\n", int(registro[2]) #print valido
-    return None
-
-
-def remover_registro(namefile, posicao):
-    '''Reescreve o registro da posição, passando um registro vazio.'''
-    escrever_arquivo(namefile, "r+b", compacta(), posicao)
+# def consultar_registro(namefile, chave, posicao, registro, TAM):
+#     with open(namefile,"rb") as arquivo:
+#         arquivo.seek(0,2)
+#         conteudo = arquivo.tell()
+#         tamanho = conteudo / TAM
+#     if arquivoExiste(namefile):
+#         with open(namefile, 'rb') as arquivo:
+#             arquivo.seek(position*(tamanho), 0)
+#             conteudo = arquivo.read(tamanho)
+#             registro = unpack("i20sic",conteudo)
+#             if chave == int(registro[0]):
+#                 print (registro[1]).decode('utf-8'), "\n", int(registro[2]) #print valido
+#     return None
+#
+#
+# def remover_registro(namefile, posicao):
+#     '''Reescreve o registro da posição, passando um registro vazio.'''
+#     escrever_arquivo(namefile, "r+b", compactarr(), posicao)
+#
--- aplicacao/run.py
+++ aplicacao/run.py
@@ -1,63 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
-from struct import *
 import arq
-import array
-import os
 
-
-def doubleHashing(chave, i, tamanho):
-    '''Calcula o DoubleHashing, usando a definição: h(c,i) = [h1(c)+i*h2(c)] mod m'''
+def doubleHashing(chave, tentativas, tamanho):
+    ''' Função Hashing. '''
     h1 = chave % tamanho
     h2 = chave // tamanho
     if h2 is 0:
         h2 = 1
-    return ((h1 + i * h2) % tamanho)
+    return ((h1 + tentativas * h2) % tamanho), tentativas+1
 
 
-def insere_registro(namefile, tamanho):
-    chave = int(input())
-    nome = input()
-    idade = int(input())
-
-    tentativas = 0
-    posicao = doubleHashing(chave, tentativas, tamanho)
-    if arq.isFile(namefile):
-
-
-def consulta_registro():
-    pass
-
-def remove_registro():
-    pass
-
-def media_acesso():
-    pass
-
-def imprime_registro():
-    pass
-
-def main(namefile, TAMANHO_ARQUIVO):
-    '''Função principal.'''
-
+def main(namefile, tamanho):
+    ''' Programa principal. '''
     opcao = input()
 
     if opcao is 'i':
-        print (insere_registro(namefile, TAMANHO_ARQUIVO))
-    elif opcao is 'c':
-        pass
-    elif opcao is 'r':
-        pass
-    elif opcao is 'p':
-        pass
-    elif opcao is 'm':
-        pass
-    elif opcao is 'e':
-        exit()
+        # recebe os valores do registro
+        chave = int(input())
+        nome = input()
+        idade =  int(input())
+        registro = arq.compactar(chave, nome, idade)
+        posicao = doubleHashing(chave, 0, tamanho) * len(registro)
+        arq.gravar_registro(namefile, tamanho, registro, posicao)
 
 
 if __name__ == '__main__':
     TAMANHO_ARQUIVO = 11
-    namefile = '_arquivo.dat'
-    main(namefile, TAMANHO_ARQUIVO)
+    ARQUIVO = 'arquivo.dat'
+    main(ARQUIVO, TAMANHO_ARQUIVO)
